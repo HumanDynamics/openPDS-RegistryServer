8cd8782a565fc32f052af57ad579526a795aaa27
Jeff <jschmitz65@gmail.com>
2012-05-24 17:52:28 -0400

    Remove unecessary files left over from Registry-Resource Separation

--- resourceServer/trustedResources/apps/account/datastoreModels.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import *
-
-class PersonalPermissions(Document):
-  overall_sharing_level = IntField(default=3)
-  roles = DictionaryField(default={})
-  uidRoles = ListField(default=[])
-  
--- resourceServer/trustedResources/apps/base/urls.py
+++
@@ -1,8 +0,0 @@
-#-*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('oauthManagement.apps.base.views',
-    (r'^static$', 'static'),
-    (r'^returnToAndroidApp$', 'returnToAndroidApp'),
-    (r'^/?$',                      'homepage'),
-)
