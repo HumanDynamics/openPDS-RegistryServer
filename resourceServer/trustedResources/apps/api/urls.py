c65741787bd4fc2227037e9b1d78dba620449ec5
Jeff <jschmitz65@gmail.com>
2012-05-24 10:33:31 -0400

    Adding in REST interfaces
    This checkin implements the REST interfaces requested in issue #39.
    
    http://50.56.241.139:10001/redmine/issues/39

--- resourceServer/trustedResources/apps/api/urls.py
+++ resourceServer/trustedResources/apps/api/urls.py
@@ -4,18 +4,13 @@ import logging, random, hashlib, string
 
 urlpatterns = patterns('trustedResources.apps.api.views',
     (r'^data$',                 'data'),
-    (r'^set_role_permissions$',     'setRolePermissions'),
-    (r'^set_sharing_level$',     'changeSharingLevel'),
-    (r'^set_funf_sensor_groups$',     'changeFunfConfig'),
-    (r'^getDefaults$',     'getDefaults'),
-    (r'^get_answers$',	'viz'),
-    (r'^validate$', 'isTokenValid'),
-    (r'^get_results$', 'viz'),
-    (r'^set_funf_data$', 'data'),
-    (r'^get_funf_sensor_data$', 'getFunfSensorData'),
-    (r'^set_reality_analysis_data$', 'setRealityAnalysisData'),
-    (r'^get_reality_analysis_data$', 'getRealityAnalysisData'),
-
+    (r'^changeRolePermissions/?$',     'changeRolePermissions'),
+    (r'^changeSharingLevel/?$',     'changeSharingLevel'),
+    (r'^changeFunfConfig/?$',     'changeFunfConfig'),
+    (r'^getDefaults/?$',     'getDefaults'),
+    (r'^viz/$',	'viz'),
+    (r'^validate/$', 'isTokenValid'),
+    (r'^get_results$', 'getResults'),
 )
 #handler404 = 'oauthManagement.apps.api.views.my_custom_404_view'
 logging.debug('testing urls...not found')
