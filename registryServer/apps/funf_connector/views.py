bcd527df2e493e06a6c0a6fcbdcdddc74796190e
brian717 <brian717@mit.edu>
2013-02-22 22:44:29 -0500

    Fixed funf connector (still on Registry) to take the name from the POSTed db name column, rather than trying to pull it out of the value. This is necessary for Funf v4

--- registryServer/apps/funf_connector/views.py
+++ registryServer/apps/funf_connector/views.py
@@ -111,58 +111,51 @@ def data(request):
     for filename, file in request.FILES.items():
         logging.debug(filename)
     if request.method == 'GET':
-	template = {'token':request.GET['bearer_token']}
-    	return render_to_response('upload.html',
-        template,
-        RequestContext(request))
+        template = {'token':request.GET['bearer_token']}
+        return render_to_response('upload.html', template, RequestContext(request))
     pds = None
     scope = AccessRange.objects.get(key="funf_write")
-#    authenticator = Authenticator(scope=scope)
     authenticator = JSONAuthenticator(scope=scope)
     try:
         # Validate the request.
         authenticator.validate(request)
     except AuthenticationException as e:
         # Return an error response.
-	print e
+        print e
         return authenticator.error_response(content="You didn't authenticate.")
     profile = authenticator.user.get_profile()
     funf_password = profile.funf_password	
 
     try:
-	    
         scope = 'funf_write'
-	token = request.GET['bearer_token']
-
-	try:
+        token = request.GET['bearer_token']
+        try:
             key = decrypt.key_from_password(str(funf_password))
-	    print key
             file_path = upload_dir + file.name
             write_file(str(file_path), file)
-	except Exception as ex:
-	    print "failed to write file to "+file_path+".  Please make sure you have write permission to the directory set in settings.SERVER_UPLOAD_DIR"
+        except Exception as ex:
+            print "failed to write file to "+file_path+".  Please make sure you have write permission to the directory set in settings.SERVER_UPLOAD_DIR"
         dbdecrypt.decrypt_if_not_db_file(file_path, key)
         con = sqlite3.connect(file_path)
         cur = con.cursor()
-        cur.execute("select value from data")
-	inserted = []
+        cur.execute("select name, value from data")
+        inserted = []
         for row in cur:
-	    json_insert = clean_keys(json.JSONDecoder().decode(convert_string(row)))
-	    print json_insert
+            name = clean_keys(convert_string(row["name"]))
+            json_insert = clean_keys(json.JSONDecoder().decode(convert_string(row["value"])))
+            print json_insert
             # Insert into PDS
-	    pds_data= {}
-	    pds_data['time']=json_insert.get('timestamp')
-	    pds_data['value']=json_insert
-	    pds_data['key']=json_insert.get('probe')
-	    insert_pds(profile, token, pds_data)
-	    print "inserting row..."
-	    print pds_data
-	    inserted.append(convert_string(json_insert)+'\n')
-	result = {'success': True, 'message':''.join(inserted)} 
-			
+            pds_data= {}
+            pds_data['time']=json_insert.get('timestamp')
+            pds_data['value']=json_insert
+            pds_data['key']=name
+            insert_pds(profile, token, pds_data)
+            print "inserting row..."
+            print pds_data
+            inserted.append(convert_string(json_insert)+'\n')
+        result = {'success': True, 'message':''.join(inserted)} 
     except Exception as e:
         result = {'success':False, 'error_message':e.message}
-
     finally:
         response_dict = {"status":"success"}
         return HttpResponse(json.dumps(response_dict), content_type='application/json')
