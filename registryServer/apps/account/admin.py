5a6a8745bf2c1918342e8f12a06c52bcfe502b90
brian717 <brian717@media.mit.edu>
2013-05-28 18:55:27 -0400

    Fixed issue with OAuth2App's admin interface displaying generic type information for models

--- registryServer/apps/account/admin.py
+++ registryServer/apps/account/admin.py
@@ -1,22 +1,29 @@
 from oauth2app.models import *
 from apps.account.models import *
 from django.contrib import admin
+from apps.oauth2.forms import AccessTokenAdminForm, CodeAdminForm
 
 admin.site.register(Profile)
 class AccessTokenAdmin(admin.ModelAdmin):
-	list_display = ('token', 'client', 'user')
-
+    list_display = ('token', 'client', 'user')
+    list_display_links = ('token', 'client', 'user')
+    form = AccessTokenAdminForm
+    
 admin.site.register(AccessToken, AccessTokenAdmin)
 class AccessRangeAdmin(admin.ModelAdmin):
-	list_display = ('key', 'description')
-	class Meta:
-		verbose_name = 'scope'
+    list_display = ('key', 'description')
+    class Meta:
+        verbose_name = 'scope'
 
 admin.site.register(AccessRange, AccessRangeAdmin)
 class ClientAdmin(admin.ModelAdmin):
-	list_display = ('name', 'description')
-
+    list_display = ('name', 'description')
+ 
 admin.site.register(Client, ClientAdmin)
-admin.site.register(Code)
+
+class CodeAdmin(admin.ModelAdmin):
+    form = CodeAdminForm
+
+admin.site.register(Code, CodeAdmin)
 admin.site.register(UserToUser)
 admin.site.register(Group)
--- registryServer/apps/oauth2/forms.py
+++ registryServer/apps/oauth2/forms.py
@@ -1,7 +1,23 @@
 #-*- coding: utf-8 -*-
+from oauth2app.models import *
+from django.contrib import admin
 
 from django import forms
 
+CLIENTS_CHOICE_FIELD = forms.ChoiceField(choices = [(client, client.name) for client in Client.objects.all()])
+SCOPES_CHOICE_FIELD = forms.MultipleChoiceField(choices = [(scope, scope.key) for scope in AccessRange.objects.all()])
+
+class AccessTokenAdminForm(forms.ModelForm):
+    class Meta:
+        model = AccessToken
+    client = CLIENTS_CHOICE_FIELD
+    scope = SCOPES_CHOICE_FIELD
+
+class CodeAdminForm(forms.ModelForm):
+    class Meta:
+        model = Code
+    client = CLIENTS_CHOICE_FIELD
+    scope = SCOPES_CHOICE_FIELD
 
 class AuthorizeForm(forms.Form):
     pass
